37dc428da98c102eaaad52018386cae00b6be78b
chanyoo0916 <yc12241@gmail.com>
2025-09-07 14:42:31 +0900

    test

--- Sparta_TeamProject_01_v2/Shop.cpp
+++ Sparta_TeamProject_01_v2/Shop.cpp
@@ -1,20 +1,24 @@
 #include <iostream>
 #include "Shop.h"
 #include "Item.h"
+#include "Inventory.h"
 #include "Character.h"
+
+
+
 
 Shop::Shop() // 상점에 아이템 추가
 {
-	availableItems.push_back(new Item("기본 검", 50, 2, Equip ));
-	availableItems.push_back(new Item("기본 활", 50 ,2, Equip));
-	availableItems.push_back(new Item("기본 단검", 50, 2, Equip));
-	availableItems.push_back(new Item("기본 지팡이", 50, 2, Equip));
-	availableItems.push_back(new Item("영웅의 검", 100, 2, Equip));
-	availableItems.push_back(new Item("영웅의 활", 100, 2, Equip));
-	availableItems.push_back(new Item("영웅의 단검", 100, 2, Equip));
-	availableItems.push_back(new Item("영웅의 지팡이", 100, 2, Equip));
+	availableItems.push_back(new Item("기본 검", 50, 1, Equip ));
+	availableItems.push_back(new Item("기본 활", 50 ,1, Equip));
+	availableItems.push_back(new Item("기본 단검", 50, 1, Equip));
+	availableItems.push_back(new Item("기본 지팡이", 50, 1, Equip));
+	availableItems.push_back(new Item("영웅의 검", 100, 1, Equip));
+	availableItems.push_back(new Item("영웅의 활", 100, 1, Equip));
+	availableItems.push_back(new Item("영웅의 단검", 100, 1, Equip));
+	availableItems.push_back(new Item("영웅의 지팡이", 100, 1, Equip));
 
-	availableItems.push_back(new Item("소형 HP 포션", 10,10, Potion));
+	availableItems.push_back(new Item("소형 HP 포션", 10, 10, Potion));
 	availableItems.push_back(new Item("소형 MP 포션", 10, 10, Potion));
 	availableItems.push_back(new Item("중형 HP 포션", 20, 10, Potion));
 
@@ -22,8 +26,8 @@ Shop::Shop() // 상점에 아이템 추가
 	availableItems.push_back(new Item("대형 HP 포션", 30, 10, Potion));
 	availableItems.push_back(new Item("대형 MP 포션", 30, 10, Potion));
 
-	availableItems.push_back(new Item("공격력 강화", 15,50, Stuff));
-	availableItems.push_back(new Item("방어력 강화", 15,50, Stuff));
+	availableItems.push_back(new Item("공격력 강화", 15, 50, Stuff));
+	availableItems.push_back(new Item("방어력 강화", 15, 50, Stuff));
 	availableItems.push_back(new Item("명중률 강화", 15, 50,Stuff));
 
 }
@@ -39,41 +43,52 @@ void Shop::displayItems() // 상점에 있는 아이템 출력
 	}
 }
 
-void Shop::buyItem(int index, Character* player) // 플레이어가 아이템 구매
-{
-	Item* item = availableItems[index];
-{
-	if (index < 0 || index >= availableItems.size())
-	{
+void Shop::buyItem(Inventory& inven) {
+	int index;
+	std::cout << "구매할 아이템 번호를 입력하세요: ";
+	std::cin >> index;
+
+	if (index < 0 || index >= availableItems.size()) {
 		std::cout << "잘못된 아이템 번호입니다.\n";
 		return;
 	}
 
-	if (player->getGold() >= item->getprice()) // 구매 가능 여부 확인
-	{
-		player->addItem(item);
-		player->spendGold(item->getprice());
-		std::cout << "당신은 " << item->getName() << " 을 구매 했습니다" << "!\n";
+	Item* item = availableItems[index];
+	int currentGold = inven.getGold();
+
+	if (currentGold >= item->getPrice()) {
+		// 아이템 복제 후 unique_ptr로 감싸서 넘기기
+		std::unique_ptr<Item> newItem = std::make_unique<Item>(*item);
+		inven.addItem(std::move(newItem));
+
+		inven.setGold(currentGold - item->getPrice());
+		std::cout << item->getName() << "을 구매했습니다!\n";
 	}
-	else
-	{
-		std::cout << "소지한 골드가 부족합니다.\n";
+	else {
+		std::cout << "골드가 부족합니다.\n";
 	}
 }
 
-void Shop::sellItem(int index, Character * player) // 플레이어가 아이템 판매
-{
-	Item* item = player->removeItem(index); // 인벤토리 아이템 제거
-	if (item) // 아이템이 존재할 경우
-	{
-		int sellPrice = static_cast<int>(item->getPrice() * 0.6); // 60% 환급
-		player->earnGold(sellPrice); 
-		availableItems.push_back(item); 
-		std::cout << "당신은 " << item->getName() << "을 " << sellPrice << " 골드에 판매했습니다.\n";
+
+
+void Shop::sellItem(Inventory& inven) {
+	int index;
+	std::cout << "판매할 아이템 번호를 입력하세요: ";
+	std::cin >> index;
+
+	Item* item = inven.findItem(index);
+	if (item) {
+		int sellPrice = static_cast<int>(item->getPrice() * 0.6);
+		int currentGold = inven.getGold();
+		inven.setGold(currentGold + sellPrice);
+
+		// 상점에 복제본을 다시 넣기
+		availableItems.push_back(new Item(*item));
+
+		inven.removeItem(index);
+		std::cout << item->getName() << "을 " << sellPrice << " 골드에 판매했습니다.\n";
 	}
-	else // 아이템이 존재하지 않을 경우
-	{
+	else {
 		std::cout << "아이템을 찾을 수 없습니다.\n";
 	}
 }
-
