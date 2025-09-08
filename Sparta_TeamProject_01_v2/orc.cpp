d16d55fda1626f8acba89a94e5e861a635788f28
lived000 <dvse2012@gmail.com>
2025-09-08 16:56:20 +0900

    Monster_Goblin_Orc
    <인선교> 2025.09.08
    
    Monster.h : level 멤버 변수 추가, performAction 반환 타입 int로 변경, getDropItem 안정성 강화, takeDamage 음수 방어 로직 추가, getter 함수 const 일관성 수정, 헤더 정리
    Monster.cpp : level을 받는 생성자로 수정, 헤더 파일에 인라인 구현된 함수 제거
    
    Goblin.h : 생성자에서 level 파라미터 받도록 수정, performAction 및 하위 행동 함수 반환 타입을 int로 통일
    Goblin.cpp : 레벨 스케일링 기능 구현, name 변수 저장 버그 수정, 모든 행동 함수가 데미지(int)를 반환하도록 로직 수정
    
    Orc.h : 생성자에서 level 파라미터 받도록 수정, performAction 및 하위 행동 함수 반환 타입을 int로 통일
    Orc.cpp : 레벨 스케일링 기능 구현, '광전사' 타입 행동 로직 수정 및 return 누락 버그 해결, 불필요한 함수(Dead Code) 제거

--- Sparta_TeamProject_01_v2/orc.cpp
+++ Sparta_TeamProject_01_v2/orc.cpp
@@ -2,42 +2,46 @@
 #include "Orc.h"
 #include <random>
 
-void Orc::performAction() 
+int Orc::performAction() 
 {
-    if (type == "야만적인") { performSavageAction(); }
-    else if (type == "광전사") { performBrutalCharge(); }
-    else { performNormalAction(); }
+    if (type == "야만적인") { return performSavageAction(); }
+    else if (type == "광전사") { return performBerserkerAction(); }
+    else { return performNormalAction(); }
 }
 
-void Orc::performNormalAction() 
+int Orc::performNormalAction() 
 {
     std::cout << name << "가 묵직한 공격을 시도합니다! 공격력: " << attack << std::endl;
+    return attack;
 }
 
-void Orc::performSavageAction() 
+int Orc::performSavageAction() 
 {
     // 30% 확률로 더 강력한 공격을 시도
     if (rand() % 100 < 30) 
     {
         std::cout << name << "가 광기에 휩싸여 무자비한 일격을 날립니다! 공격력: " << attack * 1.5 << std::endl;
+        return attack * 1.5;
     }
 
     else 
     {
-        performNormalAction();
+        return performNormalAction();
     }
 }
 
-void Orc::performBrutalCharge() 
+int Orc::performBerserkerAction() 
 {
-    // 체력이 낮아지면 강력한 돌진 공격을 사용
-    if (health <= 50) 
+    if (!isCharging) 
     {
-        std::cout << name << "가 돌진하며 당신을 맹공격합니다! 공격력: " << attack * 2 << std::endl;
+        std::cout << name << "가 돌진을 준비합니다! 다음 공격이 매우 강력해집니다!" << std::endl;
+        isCharging = true;
+        return 0; // 준비 상태, 데미지 없음
     }
-
     else 
     {
-        std::cout << name << "가 위협적인 포효를 내지릅니다." << std::endl;
+        std::cout << name << "가 돌진하며 당신을 맹공격합니다! 공격력: " << attack * 2 << std::endl;
+        isCharging = false; // 돌진 후 상태 초기화
+		return attack * 2;
     }
 }
